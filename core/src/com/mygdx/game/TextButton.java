66a349bd1322ac4574bdf49cedcee6df521f054d
toplayro <gittoromik.06@gmail.com>
2023-05-20 16:11:02 +0600

    Version 1.1

--- core/src/com/mygdx/game/TextButton.java
+++ core/src/com/mygdx/game/TextButton.java
@@ -4,7 +4,7 @@ import com.badlogic.gdx.graphics.g2d.BitmapFont;
 import com.badlogic.gdx.graphics.g2d.GlyphLayout;
 
 public class TextButton {
-    float x, y;
+    float x, y; //Comment
     float width, height;
     String text;
     BitmapFont font;
